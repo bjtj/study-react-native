cb2bb02fc22450a36ee338936b15d27454d2df72
bjtj <bjtj10@gmail.com>
2020-12-11 19:14:15 +0900

    upgraded: Calculator 0.60.3 to 0.63.4

--- Calculator/App.js
+++ Calculator/App.js
@@ -1,57 +1,58 @@
 import React, {Component, Fragment} from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput, StatusBar, } from 'react-native';
 
+/* const App: () => React$Node = () => { */
 export class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			a: 0,
-			b: 0,
-		};
-	}
-	
-	render() {
-		const {a, b} = this.state;
-		return (
-			<Fragment>
-			  <StatusBar barStyle="dark-content" />
-			  <SafeAreaView
-				style={{
-					padding: 7,
-					alignItems: 'center',
-				}}>
-				<TextInput
-				  style={{
-					  width: '100%',
-					  borderWidth: 1,
-					  borderColor: '#ececec',
-				  }}
-				  onChangeText={text => this.setState({a: parseInt(text) || 0})}>
-				  {a}
-				</TextInput>
-				<Text style={styles.text}>+</Text>
-				<TextInput
-				  style={{
-					  width: '100%',
-					  borderWidth: 1,
-					  borderColor: '#ececec',
-				  }}
-				  onChangeText={text => this.setState({b: parseInt(text) || 0})}>
-				  {b}
-				</TextInput>
-				<Text style={styles.text}>{parseInt(a) + parseInt(b)}</Text>
-			  </SafeAreaView>
-			</Fragment>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 0,
+      b: 0,
+    };
+  }
+  
+  render() {
+    const {a, b} = this.state;
+    return (
+      <Fragment>
+	<StatusBar barStyle="dark-content" />
+	<SafeAreaView
+	  style={{
+	    padding: 7,
+	    alignItems: 'center',
+	  }}>
+	  <TextInput
+	    style={{
+	      width: '100%',
+	      borderWidth: 1,
+	      borderColor: '#ececec',
+	    }}
+	    onChangeText={text => this.setState({a: parseInt(text) || 0})}>
+	    {a}
+	  </TextInput>
+	  <Text style={styles.text}>+</Text>
+	  <TextInput
+	    style={{
+	      width: '100%',
+	      borderWidth: 1,
+	      borderColor: '#ececec',
+	    }}
+	    onChangeText={text => this.setState({b: parseInt(text) || 0})}>
+	    {b}
+	  </TextInput>
+	  <Text style={styles.text}>{parseInt(a) + parseInt(b)}</Text>
+	</SafeAreaView>
+      </Fragment>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	text: {
-		fontSize: 18,
-		color: 'black',
-		margin: 7,
-	}
+  text: {
+    fontSize: 18,
+    color: 'black',
+    margin: 7,
+  }
 });
 
 export default App;
--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # React Native (RN) 익히기 #
 
+## react native upgrade ##
+
+### Upgrading to new React Native versions ###
+
+<https://reactnative.dev/docs/upgrading>
+
+<https://react-native-community.github.io/upgrade-helper/?from=0.60.3&to=0.63.4>
+
+```
+$ yarn
+$ cd ios
+$ pod install
+$ cd ..
+$ npx react-native upgrade
+```
+
+## CocoaPods could not find compatible versions for pod "Folly" #30015 ##
+
+<https://github.com/facebook/react-native/issues/30015#issuecomment-698127149>
+
+```
+pod update Folly --no-repo-update
+```
+
+
+## `/ios/Pods/Target Support Files/Yoga/Yoga.debug.xcconfig: unable to open file (in target "Yoga" in project "Pods")` ##
+
+<https://stackoverflow.com/a/60754612/5676460>
+
+```
+rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -rf node_modules && rm -rf ios/build/* && rm -rf android/build/* && rm -rf $TMPDIR/react-* && yarn cache clean && rm -rf /tmp/haste-map-react-native-packager-* && yarn
+```
+
+## `Showing Recent Messages Undefined symbol: associated type descriptor for Swift.ExpressibleByFloatLiteral.FloatLiteralType` ##
+
+<https://blog.cpming.top/p/react-native-0-62-undefined-symbol>
+
+Solution 확인
 
 ## 설치 ##
 
